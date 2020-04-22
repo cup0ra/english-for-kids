11f16154ef7b584dcabda02d38611535d7df320c
cup0ra <cup0ra@users.noreply.github.com>
2020-04-22 22:51:23 +0300

    feat: add statistic

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,22 @@
-/* eslint-disable no-param-reassign */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-use-before-define */
 /* eslint-disable import/extensions */
+/* eslint-disable no-sequences */
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+
 
 
 import '../css/style.css';
 import '../css/style.scss';
 import {actions,actions2,animals,animals2,clothes,emotions,food,itemes} from './cards.js';
+import {staticObject} from './statistic.js';
 
+function addStaticObjectToLocalStorage() {  
+  if (localStorage.length === 0){
+    localStorage.setItem ("object", JSON.stringify(staticObject))
+  }
+ 
+}
+addStaticObjectToLocalStorage()
 
 const containerCategory = document.getElementById('container');
 const container = document.querySelector('main');
@@ -15,6 +24,7 @@ const scoreContainer = document.getElementById('score-container')
 const war = containerCategory.parentNode.removeChild(containerCategory);
 container.append(war);
 const body = document.querySelector('body');
+
 
 
 class Card {
@@ -40,6 +50,10 @@ class Card {
       button.className = 'play';
       button.innerText = 'Start Game'
       container.appendChild(button); 
+      const buttonRepeat = document.createElement('div');
+      buttonRepeat.id = 'repeat';
+      buttonRepeat.className = 'repeat none';
+      container.appendChild(buttonRepeat); 
     } else{
         state.forEach ((e) => {
         const cards = document.createElement('div');
@@ -69,6 +83,8 @@ document.getElementById('toggle').addEventListener('change', () => {
   container.innerHTML = '';
       container.appendChild(war);
       scoreContainer.innerHTML=''
+      body.querySelectorAll('a').forEach(el => el.classList.remove('press-link'));
+      document.getElementById('home').classList.add('press-link');
   document.getElementById('menu').classList.toggle('container-category__item-game')
   const elements = document.getElementsByClassName("container-category__item")
  elements.forEach((e) => {
@@ -81,7 +97,33 @@ document.getElementById('toggle').addEventListener('change', () => {
     scoreContainer.innerHTML=''
    }
 
+   body.addEventListener('click',(event) => {
+    if (event.target.tagName === 'A'){ 
+      body.querySelectorAll('a').forEach(el => el.classList.remove('press-link'));
+    }
+  });
+  
 body.addEventListener('click', (event) => {
+  if(event.target.id === 'reset'){
+    localStorage.clear()
+    addStaticObjectToLocalStorage()
+    container.innerHTML = ''
+    const w = new Statistic( JSON.parse(localStorage.getItem("object"))) 
+  }
+  if(event.target.className === 'front'){
+    const objectStat = JSON.parse(localStorage.getItem("object"))
+    console.log(objectStat)
+    for(let i = 0; i < objectStat.length; i +=1){
+      for(let j = 0; j < objectStat[i].length; j +=1){
+        if (event.target.id === objectStat[i][j].word ){
+          const elem = objectStat[i][j];
+          elem.train +=1;
+        }
+      }
+    }
+    localStorage.setItem ("object", JSON.stringify(objectStat))
+  }
+  
   if (event.target.id === 'menu__toggle'){
     document.getElementById('menu').classList.toggle('active-burger');
     document.getElementById('menu-block').classList.toggle('hidden');
@@ -93,51 +135,63 @@ body.addEventListener('click', (event) => {
   }
   if(event.target.id ==='play-game'){
     game()
-    event.target.removeAttribute('id');
-    event.target.classList.remove('play')
-    event.target.classList.add('repeat')
-    event.target.innerText = "";
+    event.target.classList.add('none');
+   document.getElementById('repeat').classList.remove('none')
   }
   if (event.target.tagName === 'A' || event.target.tagName === 'IMG') {
     document.getElementById('menu-block').classList.add('hidden');
     document.getElementById('menu__toggle').checked = 0;
     scoreContainer.innerHTML = ''
-    if (event.target.id === 'home') {
+    if (event.target.dataset.id === 'home') {
       container.innerHTML = '';
       container.appendChild(war);
+      document.getElementById('home').classList.add('press-link');
     }
-    if (event.target.id === 'actions') {
+    if (event.target.dataset.id === 'actions' ) {
       container.innerHTML = '';
       const card1 = new Card(actions);
+      document.getElementById('actions').classList.add('press-link');
     }
-    if (event.target.id === 'actions2') {
+    if (event.target.dataset.id === 'actions2') {
       container.innerHTML = '';
       const card2 = new Card(actions2);
+      document.getElementById('actions2').classList.add('press-link');
     }
-    if (event.target.id === 'animals') {
+    if (event.target.dataset.id === 'animals') {
       container.innerHTML = '';
       const card3 = new Card(animals);
+      document.getElementById('animals').classList.add('press-link');
     }
-    if (event.target.id === 'animals2') {
+    if (event.target.dataset.id === 'animals2') {
       container.innerHTML = '';
       const card4 = new Card(animals2);
+      document.getElementById('animals2').classList.add('press-link');
     }
-    if (event.target.id === 'clothes') {
+    if (event.target.dataset.id === 'clothes') {
       container.innerHTML = '';
       const card5 = new Card(clothes);
+      document.getElementById('clothes').classList.add('press-link');
     }
-    if (event.target.id === 'emotions') {
+    if (event.target.dataset.id === 'emotions') {
       container.innerHTML = '';
       const card6 = new Card(emotions);
+      document.getElementById('emotions').classList.add('press-link');
       
     }
-    if (event.target.id === 'food') {
+    if (event.target.dataset.id === 'food') {
       container.innerHTML = '';
       const card7 = new Card(food);
+      document.getElementById('food').classList.add('press-link');
     }
-    if (event.target.id === 'items') {
+    if (event.target.dataset.id === 'items') {
       container.innerHTML = '';
       const card8 = new Card(itemes);
+      document.getElementById('items').classList.add('press-link');
+    }
+    if (event.target.dataset.id === 'statistic') {
+      container.innerHTML = '';
+      const stateTeble = new Statistic( JSON.parse(localStorage.getItem("object"))) 
+      document.getElementById('statistic').classList.add('press-link');
     }
   }
   if (event.target.className === 'rotate'){
@@ -179,24 +233,24 @@ function playAudio(){
   myAudio.src = audioElement[audioElement.length-1].src;
   myAudio.play()
   }
-  setTimeout(playAudio,1000)
+  playAudio()
+  document.querySelector('.repeat').addEventListener('click',() =>{
+   playAudio()
+  });
   body.addEventListener('click', (event)=>{
     if (event.target.tagName === 'A' || event.target.tagName === 'IMG') {
       isGame = false;
     }
   });
 container.addEventListener('click', (event)=>{
-  if (event.target.className === 'repeat'){
-    playAudio();
-  }
   if(event.target.className === 'front game' && isGame === true){
     if(event.target.className === 'a'){
       isGame = false;
     }
     if (audioElement.length > 1){
   if (audioElement[audioElement.length-1].dataset.word === event.target.id){
-      event.target.parentNode.parentNode.classList.add('press')
-      const ok = new Audio('audio/correct.mp3')
+       event.target.parentNode.parentNode.classList.add('press')
+       const ok = new Audio('audio/correct.mp3')
        ok.play();
        const starWin = document.createElement('div');
        starWin.className='star-win';
@@ -204,6 +258,17 @@ container.addEventListener('click', (event)=>{
        audioElement.pop();
        event.target.classList.add('not') 
       setTimeout(playAudio,1000)
+      const objectStat = JSON.parse(localStorage.getItem("object"))
+      console.log(objectStat)
+      for(let i = 0; i < objectStat.length; i +=1){
+        for(let j = 0; j < objectStat[i].length; j +=1){
+          if (event.target.id === objectStat[i][j].word ){
+            const elem = objectStat[i][j];
+            elem.play +=1;
+          }
+        }
+      }
+      localStorage.setItem ("object", JSON.stringify(objectStat))
     }
   else { 
       const bed = new Audio('audio/error.mp3')
@@ -212,6 +277,17 @@ container.addEventListener('click', (event)=>{
       star.className='star';
       scoreContainer.append(star);
       counterStar += 1;
+      const objectStat = JSON.parse(localStorage.getItem("object"))
+      console.log(objectStat)
+      for(let i = 0; i < objectStat.length; i +=1){
+        for(let j = 0; j < objectStat[i].length; j +=1){
+          if (event.target.id === objectStat[i][j].word ){
+            const elem = objectStat[i][j];
+            elem.playError +=1;
+          }
+        }
+      }
+      localStorage.setItem ("object", JSON.stringify(objectStat))
     }
   } else if (audioElement.length === 1){
     if (counterStar>0){
@@ -221,6 +297,8 @@ container.addEventListener('click', (event)=>{
       failure.play();
       setTimeout(main,3000 )
       isGame = false;
+      body.querySelectorAll('a').forEach(el => el.classList.remove('press-link'));
+      document.getElementById('home').classList.add('press-link');
     }
   else {
     container.innerHTML = ''
@@ -229,10 +307,35 @@ container.addEventListener('click', (event)=>{
     success.play();
     setTimeout(main,3000 )
     isGame = false;
+    body.querySelectorAll('a').forEach(el => el.classList.remove('press-link'));
+    document.getElementById('home').classList.add('press-link');
   }
   }
 }
 })
+}
+// STATISTIC
+function Statistic(state){
+  const result = document.createElement('div')
+  result.className = 'result'
+  container.append(result)
+  const categoryTable = ['actions','actions2','animals','animals2','clothes','emotions','food','itemes']
+  const buttonReset = document.createElement('div')
+  buttonReset.id = 'reset'
+  buttonReset.innerText = 'RESET'
+  result.append(buttonReset)
+  const table = document.createElement('table')
+  table.id = 'table'
+  result.append(table)
+  table.innerHTML = '<td>CATEGORY</td><td>WORD</td><td>TRANSLATION</td><td>TRAIN</td><td>PLAY</td><td>PLAY ERROR</td><td>%</td>'
+  for(let i = 0, k = 0; i < state.length, k < categoryTable.length; i +=1, k +=1){
+    for(let j = 0; j < state[i].length; j +=1){
+    const percent =  (state[i][j].playError/(state[i][j].playError + state[i][j].play))*100 || 0
+    const tr = document.createElement('tr')
+    tr.innerHTML = `<td>${ categoryTable[k]}</td><td>${ state[i][j].word  }</td><td>${ state[i][j].translation  }</td><td>${ state[i][j].train }</td><td>${ state[i][j].play  }</td><td>${ state[i][j].playError  }</td><td>${ percent.toFixed(1) }</td>`
+    table.append(tr)
+  }
+  }
 }
 
 
---
+++ src/js/statistic.js
@@ -0,0 +1,473 @@
+ const staticObject =[ [
+    {
+      word: 'cry',
+      translation: 'плакать',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'dance',
+      translation: 'танцевать',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'dive',
+      translation: 'нырять',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'draw',
+      translation: 'рисовать',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'fish',
+      translation: 'ловить рыбу',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'fly',
+      translation: 'летать',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'hug',
+      translation: 'обнимать',
+     train:0,
+      play: 0,
+      playError:0
+    },
+    {
+      word: 'jump',
+      translation: 'прыгать',
+     train:0,
+      play:0,
+      playError:0
+    }
+ ],
+  
+   
+   
+ [
+      {
+        word: 'open',
+        translation: 'открывать',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'play',
+        translation: 'играть',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'point',
+        translation: 'указывать',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'ride',
+        translation: 'ездить',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'run',
+        translation: 'бегать',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'sing',
+        translation: 'петь',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'skip',
+        translation: 'пропускать, прыгать',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'swim',
+        translation: 'плавать',
+       train:0,
+        play: 0,
+        playError:0
+      }
+    ],
+  
+     [
+      {
+        word: 'cat',
+        translation: 'кот',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'chick',
+        translation: 'цыплёнок',
+       train:0,
+        play:0,
+        playError:0
+        
+        
+      },
+      {
+        word: 'chicken',
+        translation: 'курица',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'dog',
+        translation: 'собака',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'horse',
+        translation: 'лошадь',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'pig',
+        translation: 'свинья',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'rabbit',
+        translation: 'кролик',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'sheep',
+        translation: 'овца',
+       train:0,
+        play: 0,
+        playError:0
+      }
+    ],
+  
+     [
+      {
+        word: 'bird',
+        translation: 'птица',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'fish',
+        translation: 'рыба',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'frog',
+        translation: 'жаба',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'giraffe',
+        translation: 'жирафа',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'lion',
+        translation: 'лев',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'mouse',
+        translation: 'мышь',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'turtle',
+        translation: 'черепаха',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'dolphin',
+        translation: 'дельфин',
+       train:0,
+        play: 0,
+        playError:0
+      }
+    ],
+  [
+      {
+        word: 'skirt',
+        translation: 'юбка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'pants',
+        translation: 'брюки',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'blouse',
+        translation: 'блузка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'dress',
+        translation: 'платье',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'boot',
+        translation: 'ботинок',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'shirt',
+        translation: 'рубашка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'coat',
+        translation: 'пальто',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'shoe',
+        translation: 'туфли',
+       train:0,
+        play:0,
+        playError:0
+      }
+    ],
+  
+   [
+      {
+        word: 'banana',
+        translation: 'банан',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'apple',
+        translation: 'яблоко',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'bread',
+        translation: 'хлеб',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'milk',
+        translation: 'молоко',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'pizza',
+        translation: 'пицца',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'tomato',
+        translation: 'испуганный',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'cheese',
+        translation: 'сыр',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'meat',
+        translation: 'мясо',
+       train:0,
+        play: 0,
+        playError:0
+      }
+    ],
+   [
+      {
+        word: 'sad',
+        translation: 'грустный',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'angry',
+        translation: 'сердитый',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'happy',
+        translation: 'счастливый',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'tired',
+        translation: 'уставший',
+       train:0,
+        play:0,
+        playError:0
+      },
+      {
+        word: 'surprised',
+        translation: 'удивлённый',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'scared',
+        translation: 'испуганный',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'smile',
+        translation: 'улыбка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'laugh',
+        translation: 'смех',
+       train:0,
+        play: 0,
+        playError:0
+      }
+    ],
+    [
+      {
+        word: 'bed',
+        translation: 'кровать',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'pen',
+        translation: 'карандаш',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'pencil',
+        translation: 'ручка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'table',
+        translation: 'стол',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'spoon',
+        translation: 'ложка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'fork',
+        translation: 'вилка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'plate',
+        translation: 'тарелка',
+       train:0,
+        play: 0,
+        playError:0
+      },
+      {
+        word: 'chair',
+        translation: 'стул',
+       train:0,
+        play: 0,
+        playError:0
+      }
+    ]]
+    export {staticObject}
